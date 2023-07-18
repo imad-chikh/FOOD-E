09b23c3d83f5bb82c54dca9ca48171a8f7a89c1f
imad <imadedinchikh@gmail.com>
2023-07-18 13:00:49 +0100

    login+register routing

--- lib/modules/splash_page/controller.dart
+++ lib/modules/splash_page/controller.dart
@@ -1,6 +1,17 @@
 import 'package:get/get.dart';
 
-class SplashController extends GetxController {
-static SplashController get to => Get.find();
+import '../../common/routes/names.dart';
 
+class SplashController extends GetxController {
+  static SplashController get to => Get.find();
+
+  @override
+  void onReady() {
+    Future.delayed(
+        const Duration(seconds: 3),
+        () => Get.offNamed(
+              AppRoutes.Auth,
+            ));
+    super.onReady();
+  }
 }
